b61bf47ef21de62a71519e56e9d98accb370830a
MiguelAvillez <mpignatelli.avillez@gmail.com>
2023-07-20 11:25:51 +0200

    Some small modifications

--- position_estimation_plotting.py
+++ position_estimation_plotting.py
@@ -24,19 +24,20 @@ def plot():
     s = 1
     lw = 0.5
     rasterize = False
-    plot_residuals = False
     plot_position_error = True
     plot_kepler_elements_error = True
+    plot_step_size = False
 
     dir = "MGS/mors_2190/estimation_position/" # /data_spice_test
 
     file_tag = "november2005"
+    # file_tag = "novDec2005"
 
     full_state_history_prefit = np.loadtxt(f"{dir}/stateHistoryPropagatedPreFit_{file_tag}.txt")
     time_history_prefit = full_state_history_prefit[:,0]
     state_history_prefit = full_state_history_prefit[:,1:]
 
-    full_state_history_postfit = np.loadtxt(f"{dir}/stateHistoryPropagatedPostFit_{file_tag}.txt")
+    full_state_history_postfit = np.loadtxt(f"{dir}/stateHistoryPropagatedPostFit_{file_tag}Tol10.txt")
     time_history_postfit = full_state_history_postfit[:,0]
     state_history_postfit = full_state_history_postfit[:,1:]
 
@@ -84,24 +85,38 @@ def plot():
     ####################################################################################################################
 
     if plot_position_error:
-        fig, ax = plt.subplots(1, 1, figsize = (6,3), constrained_layout = True)
-        ax.plot((time_history_prefit[:-10] - time_history_prefit[0]) / 86400,
-                   np.linalg.norm(state_history_prefit[:-10,0:3] - state_history_spice[:-10,0:3], axis=1) / 1e3,
+        fig, ax = plt.subplots(1, 1, figsize = (6,3), constrained_layout=True)
+        ax.plot((time_history_prefit[10:-10] - time_history_prefit[0]) / 86400,
+                   np.linalg.norm(state_history_prefit[10:-10,0:3] - state_history_spice[10:-10,0:3], axis=1) / 1e3,
                    lw=lw*2, label="Pre-fit")
         ax.plot((time_history_postfit[:-10] - time_history_postfit[0]) / 86400,
                    np.linalg.norm(state_history_postfit[:-10,0:3] - state_history_spice[:-10,0:3], axis=1) / 1e3,
                    lw=lw*2, label="Post-fit (from state)")
-        # ax.plot((residuals_time_history[:-10] - residuals_time_history[0]) / 86400,
-        #            np.linalg.norm(residuals_history[:-10,0:3], axis=1) / 1e3,
-        #            lw=lw*2, ls="--", label="Post-fit (from residuals)")
 
         ax.set_xlabel("Time [day]")
-        ax.set_ylabel("Position error [km]")
+        ax.set_ylabel("Position norm error [km]")
         ax.legend()
 
         ax.set_yscale("log")
         ax.grid()
         plt.savefig(f'{dir}/plots/{file_tag}_positionError{reference_state_tag}.pdf', bbox_inches='tight', pad_inches=0.02)
+        plt.close()
+
+        fig, ax = plt.subplots(1, 1, figsize = (6,3), constrained_layout = True)
+        # ax.plot((time_history_prefit[:-10] - time_history_prefit[0]) / 86400,
+        #         (np.linalg.norm(state_history_prefit[:-10,0:3], axis=1) - np.linalg.norm(state_history_spice[:-10,0:3], axis=1)) / 1e3,
+        #         lw=lw*2, label="Pre-fit")
+        ax.plot((time_history_postfit[:-10] - time_history_postfit[0]) / 86400,
+                (np.linalg.norm(state_history_postfit[:-10,0:3], axis=1) - np.linalg.norm(state_history_spice[:-10,0:3], axis=1)) / 1e3,
+                lw=lw*2, label="Post-fit")
+
+        ax.set_xlabel("Time [day]")
+        ax.set_ylabel("Position error [km]")
+        ax.legend()
+
+        # ax.set_yscale("log")
+        ax.grid()
+        plt.savefig(f'{dir}/plots/{file_tag}_positionNormError{reference_state_tag}.pdf', bbox_inches='tight', pad_inches=0.02)
         plt.close()
 
     ####################################################################################################################
@@ -110,7 +125,8 @@ def plot():
 
         kepler_history_spice = cartesian_to_keplerian(state_history_spice, mars_gravitational_parameter)
 
-        for state_history, out_name in zip([state_history_prefit, state_history_postfit], ["preFit", "postFit"]):
+        # for state_history, out_name in zip([state_history_prefit, state_history_postfit], ["preFit", "postFit"]):
+        for state_history, out_name in zip([state_history_postfit], ["postFit"]):
 
             kepler_history = cartesian_to_keplerian(state_history, mars_gravitational_parameter)
 
@@ -135,12 +151,36 @@ def plot():
                           (kepler_history[:-10,4] - kepler_history_spice[:-10,4]) * 180 / np.pi, lw=lw)
             axs[1,1].set_ylabel("$\Delta$ RAAN [deg]")
 
-            true_longitude = kepler_history[:,3] + kepler_history[:,4] + kepler_history[:,5]
-            true_longitude_spice = kepler_history_spice[:,3] + kepler_history_spice[:,4] + kepler_history_spice[:,5]
+            ###### True anomaly
+            # axs[1,2].plot((time_history_prefit[:-10] - time_history_prefit[0]) / 86400,
+            #               (kepler_history[:-10,5] - kepler_history_spice[:-10,5]) * 180 / np.pi, lw=lw)
+            # axs[1,2].set_ylabel("$\Delta \\theta$ [deg]")
+
+            ###### True longitude
+            # true_longitude = kepler_history[:,3] + kepler_history[:,4] + kepler_history[:,5]
+            # true_longitude_spice = kepler_history_spice[:,3] + kepler_history_spice[:,4] + kepler_history_spice[:,5]
+            #
+            # delta_true_longitude = true_longitude[:-10] - true_longitude_spice[:-10]
+            # for i in range(np.size(delta_true_longitude)):
+            #     if delta_true_longitude[i] > 0:
+            #         delta_true_longitude[i] = delta_true_longitude[i] % (2 * np.pi)
+            #     else:
+            #         delta_true_longitude[i] = delta_true_longitude[i] % (-2 * np.pi)
+            #
+            # axs[1,2].plot((time_history_prefit[:-10] - time_history_prefit[0]) / 86400,
+            #               delta_true_longitude * 180 / np.pi, lw=lw)
+            # axs[1,2].set_ylabel("$\Delta$ true long [deg]")
+
+            ##### Argument of latitude
+            argument_latitude = kepler_history[:,3] + kepler_history[:,5]
+            argument_latitude_spice = kepler_history_spice[:,3] + kepler_history_spice[:,5]
+
+            delta_argument_latitude = argument_latitude[:-10] - argument_latitude_spice[:-10]
 
             axs[1,2].plot((time_history_prefit[:-10] - time_history_prefit[0]) / 86400,
-                          (true_longitude[:-10] - true_longitude_spice[:-10]) * 180 / np.pi, lw=lw)
-            axs[1,2].set_ylabel("$\Delta$ true long [deg]")
+                          delta_argument_latitude * 180 / np.pi, lw=lw)
+            axs[1,2].set_ylabel("$\Delta$ arg latitude [deg]")
+
 
             for ax in axs.flatten():
                 ax.set_xlabel("Time [day]")
@@ -175,6 +215,24 @@ def plot():
             ax.grid()
 
         plt.savefig(f'{dir}/plots/{file_tag}_kepler.pdf', bbox_inches='tight', pad_inches=0.02)
+        plt.close()
+
+    if plot_step_size:
+        full_state_history_prefit_raw = np.loadtxt(f"{dir}/stateHistoryPropagatedPreFitRaw_{file_tag}.txt")
+        time_history_prefit_raw = full_state_history_prefit_raw[:,0]
+
+        fig, ax = plt.subplots(1, 1, figsize = (6,3), constrained_layout = True)
+        time_history = time_history_prefit_raw[10:-10]
+        step_size = time_history[1:] - time_history[:-1]
+        ax.plot((time_history[:-1] - time_history[0]) / 86400,
+                step_size, lw=lw*2)
+
+        ax.set_xlabel("Time [day]")
+        ax.set_ylabel("Step size [s]")
+
+        # ax.set_yscale("log")
+        ax.grid()
+        plt.savefig(f'{dir}/plots/{file_tag}_stepSizePreFit.pdf', bbox_inches='tight', pad_inches=0.02)
         plt.close()
 
 ########################################################################################################################
